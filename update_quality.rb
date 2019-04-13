1eaa985b8ef64ef07563588918be15db06ed94d3
Elizabeth Le <elizabethlemk@yahoo.com>
2019-04-12 20:12:09 -0400

    did it

--- update_quality.rb
+++ update_quality.rb
@@ -1,49 +1,51 @@
 require 'award'
+require 'pry'
+
 
 def update_quality(awards)
   awards.each do |award|
-    if award.name != 'Blue First' && award.name != 'Blue Compare'
-      if award.quality > 0
-        if award.name != 'Blue Distinction Plus'
-          award.quality -= 1
-        end
-      end
-    else
+    case award.name
+    when 'Blue Distinction Plus' # max at 80 and never alters
+      next
+    when 'Blue First'   # increases the older they get, max at 50; 2x decrease after expiration
       if award.quality < 50
-        award.quality += 1
-        if award.name == 'Blue Compare'
-          if award.expires_in < 11
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
-          if award.expires_in < 6
-            if award.quality < 50
-              award.quality += 1
-            end
-          end
+       award.quality += 1
+       if award.expires_in <= 0 && award.quality < 50
+         award.quality += 1
+       end
+      end
+    when 'Blue Compare' # 2x increase when < 10 days; 3x increase when < 5 days; 0x after expiration
+      if award.expires_in < 11 && award.expires_in > 5
+       if award.quality < 49
+         award.quality += 2
+       else
+         award.quality = 50
+       end
+     elsif award.expires_in < 6
+       if award.quality < 48
+         award.quality += 3
+       else
+         award.quality = 50
+       end
+     else
+       award.quality += 1
+      end
+     if award.expires_in <= 0
+       award.quality = 0
+     end
+    when 'Blue Star' #2x decrease the older they get; 2x decrease after expiration
+      if award.quality >= 2
+        award.quality -= 2
+        if award.expires_in <= 0
+          award.quality -= 2 if award.quality >= 2
         end
       end
-    end
-    if award.name != 'Blue Distinction Plus'
-      award.expires_in -= 1
-    end
-    if award.expires_in < 0
-      if award.name != 'Blue First'
-        if award.name != 'Blue Compare'
-          if award.quality > 0
-            if award.name != 'Blue Distinction Plus'
-              award.quality -= 1
-            end
-          end
-        else
-          award.quality = award.quality - award.quality
-        end
-      else
-        if award.quality < 50
-          award.quality += 1
-        end
+    else # 'Normal Award' decreases the older they get; 2x decrease after expiration
+      award.quality -= 1 if award.quality > 0
+      if award.expires_in <= 0
+        award.quality -= 1 if award.quality > 0
       end
     end
+    award.expires_in -= 1
   end
 end
